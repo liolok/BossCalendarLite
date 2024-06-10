9c75ade0cf64aa940fd66eef85afa38f3e6c9d1c
liolok <i@liolok.com>
2024-06-10 21:49:49 +0800

    refactor

--- scripts/bosscalendar/constants.lua
+++ scripts/bosscalendar/constants.lua
@@ -3,54 +3,54 @@ BOSS = { -- codename for bosses
   'crabking', 'malbatross', 'terrarium', 'daywalker',
 }
 
-INFO = {          -- all other information of bosses
-  dragonfly = {   -- "Dragonfly"
+INFO = {        -- all other information of bosses
+  dragonfly = { -- "Dragonfly"
     DROPS = { 'dragon_scales' },
     ANIMS = { 'death' },
     RESPAWN_INTERVAL = TUNING.DRAGONFLY_RESPAWN_TIME,
   },
-  beequeen = {   -- "Bee Queen"
+  beequeen = { -- "Bee Queen"
     DROPS = { 'hivehat' },
     ANIMS = { 'death' },
     RESPAWN_INTERVAL = TUNING.BEEQUEEN_RESPAWN_TIME,
   },
-  klaus_sack = {   -- "Loot Stash"
+  klaus_sack = { -- "Loot Stash"
     DROPS = { 'klaussackkey' },
     ANIMS = { 'death' },
     RESPAWN_INTERVAL = TUNING.KLAUSSACK_EVENT_RESPAWN_TIME,
   },
-  toadstool = {   -- "Toadstool"
+  toadstool = { -- "Toadstool"
     DROPS = { 'shroom_skin' },
     ANIMS = { 'death' },
     RESPAWN_INTERVAL = TUNING.TOADSTOOL_RESPAWN_TIME,
   },
-  stalker_atrium = {   -- "Fuelweaver"
+  stalker_atrium = { -- "Fuelweaver"
     DROPS = { 'skeletonhat' },
     ANIMS = { 'death3' },
     RESPAWN_INTERVAL = TUNING.ATRIUM_GATE_COOLDOWN + TUNING.ATRIUM_GATE_DESTABILIZE_TIME,
   },
-  crabking = {   -- "Crab King"
+  crabking = { -- "Crab King"
     DROPS = { 'singingshell_octave5' },
     ANIMS = { 'death2' },
     RESPAWN_INTERVAL = TUNING.CRABKING_RESPAWN_TIME,
   },
-  malbatross = {   -- "Malbatross"
+  malbatross = { -- "Malbatross"
     DROPS = { 'malbatross_beak' },
     ANIMS = { 'death_ocean', 'death' },
     RESPAWN_INTERVAL = TUNING.MALBATROSS_SPAWNDELAY_BASE + TUNING.MALBATROSS_SPAWNDELAY_RANDOM,
   },
-  terrarium = {   -- "Terrarium"
+  terrarium = { -- "Terrarium"
     DROPS = { 'eyemaskhat', 'shieldofterror' },
     ANIMS = { 'death', 'defeat' },
     RESPAWN_INTERVAL = TUNING.EYEOFTERROR_SPAWNDELAY,
   },
-  daywalker = {   -- "Nightmare Werepig"
+  daywalker = { -- "Nightmare Werepig"
     DROPS = { 'horrorfuel' },
     ANIMS = { 'defeat' },
     RESPAWN_INTERVAL = (TUNING.DAYWALKER_RESPAWN_DAYS_COUNT + 1) * TUNING.TOTAL_DAY_TIME,
     -- will subtract time spent on current day
   },
-  daywalker2 = {   -- "Scrappy Werepig"
+  daywalker2 = { -- "Scrappy Werepig"
     DROPS = { 'wagpunk_bits' },
     ANIMS = { 'defeat' },
     RESPAWN_INTERVAL = (TUNING.DAYWALKER_RESPAWN_DAYS_COUNT + 1) * TUNING.TOTAL_DAY_TIME,
